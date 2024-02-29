607edb0ad9a1a700adfbb6edf08720c5fe5f992c
alif-z <161561089+alif-z@users.noreply.github.com>
2024-02-29 15:35:12 +0800

    Update apple_quality_ready_deploy.py

--- apple_quality_ready_deploy.py
+++ apple_quality_ready_deploy.py
@@ -60,7 +60,8 @@ st.write("""
 """)
 
 import streamlit as st
-st.image('good apple.jpg', caption='good apple')
+st.image('good apple.jpg', caption='good apple',width=5, use_column_width=5)
 
+#st.image(image, caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
 
 
